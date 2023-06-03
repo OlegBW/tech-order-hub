72017b6edb43919fbe3699a6594fdbe56318e0bd
OlegBW <olegkorostilov58@gmail.com>
2023-06-03 22:03:57 +0300

    feat: Implementing the basic structure of the project (Sat, Jun  3 2023, 22:03:57).

---
+++ doc/model.sql
@@ -0,0 +1,23 @@
+CREATE TABLE product(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT UNIQUE,
+    price REAL,
+    creation_date TIMESTAMP
+);
+
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT,
+    user_name TEXT UNIQUE,
+    email TEXT UNIQUE,
+    hashed_password TEXT UNIQUE,
+    user_role TEXT
+);
+
+CREATE TABLE order_info(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id REFERENCES product(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    cashier_id REFERENCES employee(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    order_status TEXT,
+    discount REAL
+);
